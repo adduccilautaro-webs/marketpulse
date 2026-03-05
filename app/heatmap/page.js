c3b1121692b589d608f7b02599fa487867d4e167
adduccilautaro-webs <adduccilautaro@gmail.com>
2026-03-05 17:49:35 -0300

    Update page.js

--- app/heatmap/page.js
+++ app/heatmap/page.js
@@ -1,6 +1,6 @@
 // app/heatmap/page.js
 import Header from '@/components/Header'
-import HeatMap from '@/components/HeatMap'
+import HeatMap from '@/components/Heatmap'
 
 export default function HeatMapPage() {
   return (
